e42a713058db243232f72fe4c84adbb6a2f7402f
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-14 20:46:01 +0100

    implemented Add event modal

---
+++ src/components/dashboard/AddEventModal.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function AddEventModal({handleModal}) {
+    return (
+        <div className={`fixed inset-0 flex justify-center items-center bg-black bg-opacity-20`}>
+            <div className="bg-white w-[30%] -mt-24 rounded md:-mr-10 border-b-2 border-b-primary transition-all duration-200 ease-in-out">
+                <div className="modal-header flex justify-between items-center px-4 py-4 border-b-2">
+
+                    <div>
+                        <h2 className="text-lg font-poppins">Add new event</h2>
+                        <p className='text-primary'>* Youre about to create an event</p>
+                    </div>
+
+                    <div className='cursor-pointer' onClick={handleModal}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+
+                    </div>
+                </div>
+
+                <div className="modal-body py-20">
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/dashboard/EventManagement.jsx
+++ src/pages/dashboard/EventManagement.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AddEventModal from '../../components/dashboard/AddEventModal'
 
 export default function EventManagement() {
+
+    const [ modal, setModal ]= useState(false)
+
+    const handleModal = () => {
+        setModal(!modal)
+    }
+
     return (
         <div className="container mx-auto h-screen px-4 md:px-6 mt-8">
 
-            <div className="header w-full  flex flex-col gap-3 justify-between md:flex-row md:px-6">
-            <h1 className="text-2xl font-raleway font-semibold">Event Management</h1>
-            <button className='border border-primary py-3 px-3 text-primary rounded-md flex flex-shrink items-center gap-2'>
-                Create Event
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-  <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
-</svg>
+            {modal && (
+            <AddEventModal handleModal={handleModal} />
 
-            </button>
+            )}
+        
+            <div className="header w-full  flex flex-col gap-3 justify-between md:flex-row md:px-6">
+                <h1 className="text-2xl font-raleway font-semibold">Event Management</h1>
+                <button className='border border-primary py-3 px-3 text-primary rounded-md flex flex-shrink items-center gap-2' onClick={handleModal}>
+                    Create Event
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                        <path d="M10.75 4.75a.75.75 0 0 0-1.5 0v4.5h-4.5a.75.75 0 0 0 0 1.5h4.5v4.5a.75.75 0 0 0 1.5 0v-4.5h4.5a.75.75 0 0 0 0-1.5h-4.5v-4.5Z" />
+                    </svg>
+
+                </button>
             </div>
         </div>
     )
