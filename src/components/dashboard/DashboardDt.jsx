277c11be7ca155dd530f8f6ae209fac86c54856d
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-14 23:19:16 +0100

    Created Table in home component

--- src/components/dashboard/DashboardDt.jsx
+++ src/components/dashboard/DashboardDt.jsx
@@ -2,9 +2,65 @@ import React from 'react'
 
 
 export default function DashboardDt() {
+
+    const registeredUsers = [
+        {
+          id: 1,
+          name: "John Doe",
+          email: "johndoe@example.com",
+         
+          // Add more details if needed (e.g., company, role, status)
+        },
+        {
+          id: 2,
+          name: "Jane Smith",
+          email: "janesmith@example.com",
+       
+          // Add more details if needed
+        },
+        {
+          id: 3,
+          name: "Michael Brown",
+          email: "michaelbrown@example.com",
+         
+          // Add more details if needed
+        },
+        // Add more users as needed
+      ];
+
+      
     return (
         <div className='w-full bg-white shadow pt-6 px-4 text-sm'>
-            
+
+            <h3 className='pb-4 px-4'>Recent ticket purchases</h3>
+            <div className="overflow-x-auto mx-auto">
+  <table className="table-auto w-full borderless">
+    <thead>
+      <tr>
+        <th className="px-4 py-2 text-left font-medium text-gray-500 tracking-wider">Name</th>
+        <th className="px-4 py-2 text-left font-medium text-gray-500 tracking-wider">Email</th>
+        <th className="px-4 py-2 text-left font-medium text-gray-500 tracking-wider">Type</th>
+      </tr>
+    </thead>
+    <tbody>
+      {registeredUsers.map((user) => (
+        <tr key={user.id} className="border-b">
+          <td className="px-4 py-4 text-gray-700">
+            {user.name}
+          </td>
+          <td className="px-4 py-4 text-gray-700">
+            {user.email}
+          </td>
+         
+          <td className="px-4 py-4 text-right text-green-500">
+            Regular
+             </td>
+             </tr>
+             ))}
+             </tbody>
+             </table>
+
+             </div>
         </div>
     )
 }
--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -31,7 +31,7 @@ export default function Sidebar() {
                     
                 </NavLink>
 
-                <NavLink to='/dashboard/events' className='text-lg flex gap-2 cursor-pointer hover:translate-x-2 transition duration-200 ease-in-out'>
+                <NavLink to='/dashboard/events' className='side-bar-item'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round  " strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                     </svg>
@@ -40,7 +40,7 @@ export default function Sidebar() {
 
                   
                 </NavLink>
-                <NavLink to='/dashboard/ticketing' className='text-lg flex gap-2 cursor-pointer hover:translate-x-2 transition duration-200 ease-in-out'>
+                <NavLink to='/dashboard/ticketing' className='side-bar-item'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                     </svg>
@@ -50,7 +50,7 @@ export default function Sidebar() {
 
                     
                 </NavLink>
-                <NavLink to='/dashboard/bookings' className='text-lg flex gap-2 cursor-pointer hover:translate-x-2 transition duration-200 ease-in-out'>
+                <NavLink to='/dashboard/bookings' className='side-bar-item'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
                     </svg>
@@ -58,8 +58,17 @@ export default function Sidebar() {
                         Bookings
 
                 </NavLink>
+                <NavLink to='/dashboard/merch' className='side-bar-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+</svg>
 
-                <NavLink to='/dashboard/payment' className='text-lg flex gap-2 cursor-pointer hover:translate-x-2 transition duration-200 ease-in-out'>
+
+                        Merch store
+
+                </NavLink>
+
+                <NavLink to='/dashboard/payment' className='side-bar-item'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                     </svg>
