40f85b358c86ff6f4c26de2f861e54408266b84d
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-14 16:27:18 +0100

    Worked on nav responsiveness

--- src/pages/dashboard/Nav.jsx
+++ src/pages/dashboard/Nav.jsx
@@ -9,7 +9,18 @@ function Nav() {
         <div className="container mx-auto flex justify-between py-2">
             <div className="navbrand text-xl font-raleway font-semibold flex w-full gap-5 justify-between items-center">
                
-                <h2>Welcome back oborovorio Johnstone</h2>
+                <h2 className='hidden md:flex'>Welcome back oborovorio Johnstone</h2>
+
+                <div className="flex gap-3 items-center md:hidden">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+                <h2 className='flex md:hidden font-raleway'>Events <span className='text-primary font-semibold'>NG</span></h2>
+
+                </div>
+
 
                 <div className='flex items-center'>
                 <div className='bg-primary p-4 rounded-full'>
