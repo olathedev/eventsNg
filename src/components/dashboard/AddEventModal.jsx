d869126efece6ffd9aa668ae2af3f4facb1aad63
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-15 23:50:58 +0100

    modify add events moddal

--- src/components/dashboard/AddEventModal.jsx
+++ src/components/dashboard/AddEventModal.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function AddEventModal({ handleModal }) {
     return (
-        <div className={`fixed inset-0 flex justify-center items-center bg-black bg-opacity-20 backdrop-blur-sm z-30 px-2`}>
-            <div className="bg-white md:w-[40%] rounded md:-mr-10 border-b-2 border-b-primary transition-all duration-200 ease-in-out">
-                <div className="modal-header flex gap-5 justify-between items-center px-4 py-4 border-b-2">
+        <div className={`fixed inset-0 flex justify-center items-center bg-black bg-opacity-40  z-30 px-2`}>
+            <div className="bg-white md:w-[35%] rounded-xl md:-mr-10 border-b-2 border-b-primary transition-all duration-200 ease-in-out">
+                <div className="modal-header flex gap-5 justify-between items-center px-6 py-6 border-b-2">
 
                     <div>
-                        <h2 className="text-lg font-poppins">Add new event</h2>
-                        <p className='text-primary'>* Youre about to create an event</p>
+                        <h2 className="text-lg font-poppins font-semibold  opacity-80">Add new event</h2>
                     </div>
 
                     <div className='cursor-pointer' onClick={handleModal}>
@@ -22,9 +21,9 @@ export default function AddEventModal({ handleModal }) {
                 <div className="modal-body py-4 px-6">
                     <form action="" className='container flex flex-col gap-6 font-monteserat'>
                         <div className="relative">
-                            <input type="text" className='w-full border-b border-gray-400 px-1 py-1 focus:outline-none' placeholder='event name ' />
+                            <input type="text" className='w-full border border-gray-400 px-3 py-3 rounded focus:outline-none' placeholder='event name ' />
 
-                            <span className="absolute right-2">
+                            <span className="absolute right-3 top-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                                 </svg>
@@ -33,9 +32,9 @@ export default function AddEventModal({ handleModal }) {
                         </div>
 
                         <div className='relative'>
-                            <input type="text" className='w-full border-b border-gray-400 px-1 py-1 focus:outline-none' placeholder='event location ' />
+                            <input type="text" className='w-full  border border-gray-400 px-3 py-3 rounded focus:outline-none' placeholder='event location ' />
 
-                            <span className='absolute right-2'>
+                            <span className='absolute right-3 top-3'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
@@ -46,9 +45,9 @@ export default function AddEventModal({ handleModal }) {
 
 
                         <div className="flex gap-4 text-sm">
-                            <input type="date" className='w-1/2 border-b  border-gray-400 px-1 py-1 focus:outline-none' placeholder='date ' />
+                            <input type="date" className='w-1/2  border border-gray-400 px-3 py-3 rounded focus:outline-none' placeholder='date ' />
 
-                            <input type="time" className='w-1/2 border-b border-gray-400 px-1 py-1 focus:outline-none' placeholder='time ' />
+                            <input type="time" className='w-1/2 border-b  border border-gray-400 px-3 py-3 rounded focus:outline-none' placeholder='time ' />
 
                         </div>
 
@@ -67,15 +66,25 @@ export default function AddEventModal({ handleModal }) {
                         </div>
 
                         <div>
-                        <div className='flex gap-6'>
+                        <div className='flex gap-6 relative'>
                             
-                            <input type="number"  className='w-1/2 border-b border-gray-400 px-1 py-1 focus:outline-none' placeholder='available seats' />
+                            <input type="number"  className='w-1/2 border border-gray-400 px-3 py-3 rounded focus:outline-none' placeholder='available seats' />
 
-                                <select className='w-1/2 border-b border-gray-400 px-1 py-1 focus:outline-none' name="">
+
+                            
+                                <select className='w-1/2 border border-gray-400 px-3 py-3 rounded appearance-none focus:outline-none' name="">
                                     <option value="" >Private event</option>
                                     <option value="">Public event</option>
-                                </select>
 
+                                   
+                                </select>
+                                
+                                <span className="absolute right-3 top-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+
+                                </span>
                         </div>
                         <p className="text-sm mt-2 text-primary">Public events are listed on events.ng and anody can find them</p>
 
@@ -83,6 +92,10 @@ export default function AddEventModal({ handleModal }) {
                         
 
                     </form>
+
+                    <div className="w-full modal-footer mt-4">
+                        <button className='w-full py-3 px-4 bg-primary text-white rounded-md'>Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
