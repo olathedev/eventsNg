b039722674e477e0292851715f6133e0b24c9ea0
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-18 12:39:13 +0100

    BugFix

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Dashboard from './pages/dashboard/Dashboard'
 import { useAuthContext } from './hooks/useAuthContext'
 
 
-axios.defaults.baseURL = "http://localhost:4000/api/v1/eventsng"
-// axios.defaults.baseURL = "https://eventsng-v1.onrender.com/api/v1/eventsng"
+// axios.defaults.baseURL = "http://localhost:4000/api/v1/eventsng"
+axios.defaults.baseURL = "https://eventsng-v1.onrender.com/api/v1/eventsng"
 
 
 function App() {
