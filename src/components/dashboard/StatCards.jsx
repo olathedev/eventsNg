884c5cbd998671505f6ab9ba534c62c892b5ccf9
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-18 03:23:55 +0100

    uiFix

--- src/components/dashboard/StatCards.jsx
+++ src/components/dashboard/StatCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function StatCards({stats}) {
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3'>
-            <div className='p-6 bg-white shadow-sm rounded'>
+            <div className='py-4 px-5 bg-white shadow-sm rounded'>
                 <div className="flex flex-col">
 
 
@@ -12,9 +12,9 @@ export default function StatCards({stats}) {
                     <h1 className='font-raleway text-2xl font-bold'>{stats?.isActive + stats?.notActive}</h1>
 
                     <div className="flex mt-2 justify-between items-center">
-                        <div className='text-primary font-semibold'>open</div>
+                        <div className='text-primary font-semibold'></div>
 
-                        <div className="flex bg-primary bg-opacity-30 p-3 rounded-md">
+                        <div className="flex bg-primary bg-opacity-30 p-3 rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                             </svg>
@@ -27,7 +27,7 @@ export default function StatCards({stats}) {
             </div>
 
 
-            <div className='p-6 bg-white shadow-sm rounded'>
+            <div className='py-4 px-5 bg-white shadow-sm rounded'>
                 <div className="flex flex-col">
 
 
@@ -38,7 +38,7 @@ export default function StatCards({stats}) {
                     <div className="flex mt-2 justify-between items-center">
                         <div className='text-primary font-semibold'></div>
 
-                        <div className="flex bg-green-700 bg-opacity-30 p-3 rounded-md">
+                        <div className="flex bg-primary bg-opacity-30 p-3 rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
                             </svg>
@@ -51,18 +51,18 @@ export default function StatCards({stats}) {
                 </div>
             </div>
 
-            <div className='p-6 bg-white shadow-sm rounded'>
+            <div className='py-4 px-5 bg-white shadow-sm rounded'>
                 <div className="flex flex-col">
 
 
 
                     <h4 className='font-poppins'>Tickets created</h4>
-                    <h1 className='font-raleway text-2xl font-bold'>20</h1>
+                    <h1 className='font-raleway text-2xl font-bold'>{stats?.tickets}</h1>
+                    
 
                     <div className="flex mt-2 justify-between items-center">
-                        <div className='text-primary font-semibold'>open</div>
-
-                        <div className="flex bg-black bg-opacity-30 p-3 rounded-md">
+                    <div className='text-primary font-semibold'></div>
+                        <div className="flex bg-primary bg-opacity-30 p-3 rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                             </svg>
@@ -75,18 +75,18 @@ export default function StatCards({stats}) {
                 </div>
             </div>
 
-            <div className='p-6 bg-white shadow-sm rounded'>
+            <div className='py-4 px-5 bg-white shadow-sm rounded'>
                 <div className="flex flex-col">
 
 
 
-                    <h4 className='font-poppins'>Tickets created</h4>
+                    <h4 className='font-poppins'>Total ticket sales</h4>
                     <h1 className='font-raleway text-2xl font-bold'>20</h1>
 
                     <div className="flex mt-2 justify-between items-center">
-                        <div className='text-primary font-semibold'>open</div>
+                        <div className='text-primary font-semibold'></div>
 
-                        <div className="flex bg-purple-900 bg-opacity-30 p-3 rounded-md">
+                        <div className="flex bg-primary bg-opacity-30 p-3 rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
                             </svg>
--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -8,7 +8,6 @@ export default function Home({stats}) {
   return (
     <div className="container mx-auto h-screen px-4 md:px-6 mt-6">
 
-            <h1 className='text-lg font-monteserat font-semibold mb-6 pl-2 text-gray-700'>Dashboard</h1>
               <StatCards stats={stats} />
               <section className='w-full mt-4 grid grid-cols-1 md:grid-cols-2 gap-4'>
                   <DashboardDt />
