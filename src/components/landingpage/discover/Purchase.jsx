61462df48a9908368184df86691b42eec5cfab3f
DevOlaMicheal <olapademicheal3@gmail.com>
2024-03-04 01:07:32 +0100

    bugFix

--- src/components/landingpage/discover/Purchase.jsx
+++ src/components/landingpage/discover/Purchase.jsx
@@ -10,7 +10,7 @@ function Purchase({ data, handleModal }) {
                 </header>
 
                 <div className="p-4 flex">
-                    <div className="md:hidden bg-primary bg-opacity-30 py-1 px-4 rounded flex gap-1 items-center">
+                    <div className="md:hidden bg-primary text-primary bg-opacity-30 py-1 px-4 rounded flex gap-1 items-center" onClick={handleModal}>
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                 <path fillRule="evenodd" d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z" clipRule="evenodd" />
